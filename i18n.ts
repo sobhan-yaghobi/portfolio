18da95f61965fbbb3f30b0de3cd92eb0439f047d
sobhan <sobhan.yaghobi.work@gmail.com>
2024-05-22 16:44:56 +0330

    inital next-intl for multiple language

---
+++ i18n.ts
@@ -0,0 +1,15 @@
+import { locales } from "@/utils/utils.variable"
+import { getRequestConfig } from "next-intl/server"
+import { cookies } from "next/headers"
+
+export default getRequestConfig(async () => {
+  const locale = cookies().get("NEXT_LOCALE")?.value
+  //! For Set Default Language If User Using NotSupported Language
+  const mainLocale =
+    locale && locales.includes(locale) ? locale : (process.env.NEXT_PUBLIC_DEF_LANGUAGE as string)
+
+  return {
+    locale,
+    messages: (await import(`@/messages/${mainLocale}.json`)).default,
+  }
+})
