250576a7e0be5ebaed6c8f04d7d45b7b9be0c572
sobhan <sobhan.yaghobi.work@gmail.com>
2024-05-22 15:02:55 +0330

    build : inital the Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import Header from "./components/modules/Header"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="dark">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
