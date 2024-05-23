e7b71d305e65b8e51301f01bb8db72c415750234
sobhan <sobhan.yaghobi.work@gmail.com>
2024-05-23 23:15:40 +0330

    change the buttonTheme structure

--- app/components/modules/ChangeTheme.button.tsx
+++ app/components/modules/ChangeTheme.button.tsx
@@ -1,41 +1,13 @@
-"use client"
-
-import React, { useEffect, useState } from "react"
-import { getLocalStorage, setLocalStorage } from "@/utils/utils.function"
-
+import React from "react"
 import { Moon, Sun } from "lucide-react"
 
 const ChangeThemeButton = () => {
-  const [isDarkTheme, setIsDarkTheme] = useState(true)
-  const updateHtmlTheme = (theme: string) =>
-    document.querySelector("html")!.setAttribute("data-theme", theme)
-
-  const changeThemeAction = () => {
-    setIsDarkTheme((prev) => {
-      const isDark = !prev
-      updateHtmlTheme(!isDark ? "light" : "dark")
-      setLocalStorage("darkTheme", JSON.stringify(isDark))
-      return isDark
-    })
-  }
-  useEffect(() => {
-    const themeLocalStorage = JSON.parse(getLocalStorage("darkTheme") as string)
-    if (typeof themeLocalStorage === "boolean") {
-      if (themeLocalStorage) {
-        setIsDarkTheme(true)
-        updateHtmlTheme("dark")
-      } else {
-        setIsDarkTheme(false)
-        updateHtmlTheme("light")
-        console.log("update")
-      }
-    } else setLocalStorage("darkTheme", JSON.stringify(true))
-  }, [])
-
   return (
-    <button className="btn btn-ghost btn-sm" onClick={changeThemeAction}>
-      {isDarkTheme ? <Sun className="icon" /> : <Moon className="icon" />}
-    </button>
+    <label className="swap swap-rotate btn btn-ghost btn-sm">
+      <input type="checkbox" className="theme-controller" value={"light"} />
+      <Sun className="icon swap-off" />
+      <Moon className="icon swap-on" />
+    </label>
   )
 }
 
--- utils/utils.function.ts
+++
@@ -1,2 +0,0 @@
-export const getLocalStorage = (key: string) => localStorage.getItem(key)
-export const setLocalStorage = (key: string, value: string) => localStorage.setItem(key, value)
