1a9bd03e988356277e17f6d7eee305c045a47058
sobhan-yagobi <sobhan.yaghobi.work@gmail.com>
2024-06-02 23:05:25 +0330

    update ContactMe.button => write the logical action

--- app/components/templates/ContactMe.button.tsx
+++ app/components/templates/ContactMe.button.tsx
@@ -1,14 +1,24 @@
 "use client"
 
 import { isUserUseMobile } from "@/utils/utils.function"
-import { Phone } from "lucide-react"
+import { ClipboardCheck, Dock, Phone } from "lucide-react"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 import { useTranslations } from "use-intl"
 
 const ContactMeButton = () => {
+  const phoneNumber = "09396007232"
   const [isMobile, setIsMobile] = useState(false)
   const t = useTranslations("heroSection")
+  const [isClipboard, setIsClipboard] = useState(false)
+
+  const saveClipboardAction = async () => {
+    await window.navigator.clipboard.writeText(phoneNumber)
+    setIsClipboard(true)
+    setTimeout(() => {
+      setIsClipboard(false)
+    }, 2000)
+  }
 
   useEffect(() => {
     const userAgent = window.navigator.userAgent ?? ""
@@ -16,14 +26,23 @@ const ContactMeButton = () => {
   }, [])
 
   return isMobile ? (
-    <Link className="btn btn-primary" href="tel:09396007232">
+    <Link className="btn btn-primary" href={`tel:${phoneNumber}`}>
       {t("quickAccessButton.contact")}
       <Phone className="icon" />
     </Link>
   ) : (
-    <button className="btn btn-primary">
-      {t("quickAccessButton.contact")}
-      <Phone className="icon" />
+    <button disabled={isClipboard} onClick={saveClipboardAction} className="btn btn-primary">
+      {isClipboard ? (
+        <>
+          {t("quickAccessButton.contactSaveSuccess")}
+          <ClipboardCheck className="icon" />
+        </>
+      ) : (
+        <>
+          {t("quickAccessButton.contact")}
+          <Phone className="icon" />
+        </>
+      )}
     </button>
   )
 }
