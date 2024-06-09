c545d48302892e38ae0a63628426bf5fe080b1ef
sobhan-yagobi <sobhan.yaghobi.work@gmail.com>
2024-06-09 18:25:55 +0330

    update => ScrollSmooth

--- app/components/templates/ScrollSmooth.tsx
+++ app/components/templates/ScrollSmooth.tsx
@@ -5,9 +5,11 @@ import React from "react"
 
 const ScrollSmooth: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <ReactLenis root options={{ lerp: 0.03, duration: 1.5, smoothWheel: true }}>
-      {children}
-    </ReactLenis>
+    <>
+      <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+        {children}
+      </ReactLenis>
+    </>
   )
 }
 
