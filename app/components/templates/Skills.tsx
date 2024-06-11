9ab5db7f719f283b398d4b47dd5a0fedb73d1fed
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-06-11 12:56:26 +0330

    update => some icon packages & build => skills section template's & install => uuid package

--- app/components/templates/Skills.tsx
+++ app/components/templates/Skills.tsx
@@ -1,170 +1,189 @@
-import React from "react"
+"use client"
+
+import React, { useState } from "react"
 import Title from "../modules/Title"
 import Image from "next/image"
 
+import { v4 as uuid } from "uuid"
+import Link from "next/link"
+import { ArrowUpRight } from "lucide-react"
+
+const skills = [
+  {
+    id: uuid(),
+    name: "HTML",
+    src: "/packages/html.svg",
+    content: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis necessitatibus eum
+          delectus sequi consequuntur dicta, itaque placeat maxime temporibus accusamus provident
+          corporis quisquam dolore repudiandae enim omnis laboriosam voluptatem corrupti.
+        </p>
+        <div className="my-3">
+          <h3 className="text-xl font-medium">Usage in this project's</h3>
+          <ul className="mt-2 flex flex-wrap gap-2">
+            <li className="btn btn-link">project 1</li>
+            <li className="btn btn-link">project 2</li>
+            <li className="btn btn-link">project 3</li>
+            <li className="btn btn-link">project 4</li>
+            <li className="btn btn-link">project 5</li>
+            <li className="btn btn-link">project 6</li>
+            <li className="btn btn-link">project 7</li>
+            <li className="btn btn-link">project 8</li>
+            <li className="btn btn-link">project 9</li>
+          </ul>
+        </div>
+        <Link href={"/"} className="btn">
+          Read More
+          <ArrowUpRight className="icon" />
+        </Link>
+      </>
+    ),
+  },
+  {
+    id: uuid(),
+    name: "CSS",
+    src: "/packages/css.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "SASS",
+    src: "/packages/sass.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "BootStrap",
+    src: "/packages/bootstrap.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "JavaScript",
+    src: "/packages/js.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "JavaScript",
+    src: "/packages/ts.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "React Js",
+    src: "/packages/react.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Next Js",
+    src: "/packages/next-js.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Prisma",
+    src: "/packages/prisma.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "SupaBase",
+    src: "/packages/supabase.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Zod",
+    src: "/packages/zod.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "React Hook Form",
+    src: "/packages/react-hook-form.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Lodash",
+    src: "/packages/lodash.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Material UI",
+    src: "/packages/material-ui.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Ant Design",
+    src: "/packages/ant-design.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "NPM",
+    src: "/packages/npm.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Postman",
+    src: "/packages/postman.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Tailwind CSS",
+    src: "/packages/tailwind.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Vite",
+    src: "/packages/vite.svg",
+    content: <></>,
+  },
+  {
+    id: uuid(),
+    name: "Figma",
+    src: "/packages/figma.svg",
+    content: <></>,
+  },
+]
+
 const Skills = () => {
+  const [mainSkill, setMainSkill] = useState(skills.at(0))
   return (
     <>
       <div className="mt-40" />
       <Title size="lg" className="text-center">
         <h2>Skills</h2>
       </Title>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 my-12">
         <ul className="flex flex-wrap gap-3">
-          <li className="badge badge-lg gap-1 py-5">
-            <Image className="icon" width={30} height={30} src="/packages/html.svg" alt="package" />
-            HTML
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image className="icon" width={30} height={30} src="/packages/css.svg" alt="package" />
-            CSS
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image className="icon" width={30} height={30} src="/packages/sass.svg" alt="package" />
-            SASS
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/bootstrap.svg"
-              alt="package"
-            />
-            BootStrap
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image className="icon" width={30} height={30} src="/packages/js.svg" alt="package" />
-            JavaScript
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image className="icon" width={30} height={30} src="/packages/ts.svg" alt="package" />
-            TypeScript
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/react.svg"
-              alt="package"
-            />
-            React Js
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/next-js.svg"
-              alt="package"
-            />
-            Next Js
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/prisma.svg"
-              alt="package"
-            />
-            Prisma
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/supabase.svg"
-              alt="package"
-            />
-            SupaBase
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image className="icon" width={30} height={30} src="/packages/zod.svg" alt="package" />
-            Zod
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/react-hook-form.svg"
-              alt="package"
-            />
-            React Hook Form
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/lodash.svg"
-              alt="package"
-            />
-            Lodash
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/material-ui.svg"
-              alt="package"
-            />
-            Material UI
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/ant-design.svg"
-              alt="package"
-            />
-            Ant Design
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image className="icon" width={30} height={30} src="/packages/npm.svg" alt="package" />
-            NPM
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/postman.svg"
-              alt="package"
-            />
-            Postman
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/tailwind.svg"
-              alt="package"
-            />
-            Tailwind CSS
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image className="icon" width={30} height={30} src="/packages/vite.svg" alt="package" />
-            Vite
-          </li>
-          <li className="badge badge-lg gap-1 py-5">
-            <Image
-              className="icon"
-              width={30}
-              height={30}
-              src="/packages/figma.svg"
-              alt="package"
-            />
-            Figma
-          </li>
+          {skills.map((item) => (
+            <li key={item.id}>
+              <button
+                className={`btn ${
+                  item.id === mainSkill?.id
+                    ? "outline-active hover:outline-active"
+                    : "hover:outline-active"
+                }`}
+                onClick={() => setMainSkill(item)}
+              >
+                <Image className="icon" width={30} height={30} src={item.src} alt={item.name} />
+                {item.name}
+              </button>
+            </li>
+          ))}
         </ul>
-        <nav className="">nav</nav>
+        <nav>{mainSkill?.content}</nav>
       </div>
     </>
   )
