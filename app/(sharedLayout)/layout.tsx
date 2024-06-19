9f89d3ba3fb099118f35cf2b8216d010c8bd2730
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-06-19 16:24:08 +0330

    clean => layout of sharedLayout folder

--- app/(sharedLayout)/layout.tsx
+++ app/(sharedLayout)/layout.tsx
@@ -1,6 +1,6 @@
-import Footer from "@/components/modules/Footer"
-import Header from "@/components/modules/Header/Header"
 import React from "react"
+import Header from "@/components/modules/Header/Header"
+import Footer from "@/components/modules/Footer"
 
 const layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
