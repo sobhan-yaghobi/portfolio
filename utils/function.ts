5afe02efae48d0a469017cee0dfba51b7e7335a9
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-07-13 16:22:54 +0330

    update => add property to random function

--- utils/function.ts
+++ utils/function.ts
@@ -2,7 +2,10 @@ import { type ClassValue, clsx } from "clsx"
 import gsap from "gsap"
 import { twMerge } from "tailwind-merge"
 
-export const random = (min: number, max: number) => gsap.utils.random(min, max)
+export const random = (min: number, max: number, isFloor?: boolean) => {
+  const number = gsap.utils.random(min, max)
+  return isFloor ? Math.floor(number) : number
+}
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
