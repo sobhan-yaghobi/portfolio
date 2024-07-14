2fae49c03117fb7a4ee6b241999959963a8c86db
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-07-14 09:59:27 +0330

    update => experienceAndArticle section in home page

--- components/templates/ExperienceAndArticle.tsx
+++ components/templates/ExperienceAndArticle.tsx
@@ -1,16 +1,15 @@
 import React from "react"
 import Title from "../modules/Title"
 import ExperienceAndArticleCard from "../modules/ExperienceAndArticle.card"
+import { getTranslations } from "next-intl/server"
 
-const ExperienceAndArticle = () => {
+const ExperienceAndArticle = async () => {
+  const t = await getTranslations("home.article")
   return (
     <section>
       <Title size="lg" className="text-center">
-        <h2>Experience & Article</h2>
-        <h4 className="w-10/12 text-sm mt-3 inline-block md:w-8/12 lg:w-4/12">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus soluta beatae in ducimus
-          omnis facilis asperiores!
-        </h4>
+        <h2>{t("title")}</h2>
+        <h4 className="w-10/12 text-sm mt-3 inline-block md:w-8/12 lg:w-4/12">{t("desc")}</h4>
       </Title>
       <div className="my-12 flex gap-10 max-lg:overflow-x-auto lg:grid lg:grid-cols-3 xl:grid-cols-4">
         {Array(4)
