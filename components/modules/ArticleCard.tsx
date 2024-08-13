505612e8a732086cf7ca862aca4e285033b01788
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-08-13 12:08:08 +0330

    update => ArticleCard style

--- components/modules/ArticleCard.tsx
+++ components/modules/ArticleCard.tsx
@@ -5,34 +5,43 @@ import { TypeArticleCardProps } from "@/lib/types/article"
 import { Heart, MessageSquareText, Send } from "lucide-react"
 
 import Image from "next/image"
+import { getWords } from "@/lib/utils"
 
 const ArticleCard: React.FC<TypeArticleCardProps> = ({ article }) => {
+  const isTitleSummary = article.title !== getWords(article.title, 6)
+  const isDescriptionSummary = article.description !== getWords(article.description, 20)
   return (
     <div className="card bg-base-200 min-w-72 max-w-full h-[470px] flex-1 shadow-xl">
-      <figure className="h-3/6 p-2 center relative overflow-visible">
-        <Image
-          width={300}
-          height={300}
-          src="/image/article.jpg"
-          className="w-full h-full min-size-60 object-cover rounded-xl"
-          alt="project-image"
-        />
+      <figure className="h-52 p-2 relative overflow-visible">
+        <div className="bg-base-300 size-full flex items-center justify-center rounded-xl overflow-hidden">
+          <Image
+            width={300}
+            height={300}
+            src="/image/article.jpg"
+            className="w-full h-full object-cover"
+            alt="article-image"
+          />
+        </div>
         <div className="w-full flex justify-evenly absolute -bottom-4">
           <p className={`glass h-12 px-4 center gap-2 rounded-lg cursor-default`}>
-            <Heart className="icon" />
-            32.2K
+            <Heart className="icon" />0
           </p>
           <p className={`glass h-12 px-4 center gap-2 rounded-lg cursor-default`}>
-            <MessageSquareText className="icon" />
-            1.4K
+            <MessageSquareText className="icon" />0
           </p>
         </div>
       </figure>
-      <div className="card-body px-3 pb-3 pt-8">
-        <h3 className="card-title text-2xl">Hello World</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dicta molestias ipsum
-          incidunt provident!
+      <div className="card-body px-3 pb-3 pt-8 justify-between">
+        <h3
+          style={{ lineHeight: "1.5em", maxHeight: "3em" }}
+          className="text-xl font-dana overflow-hidden"
+        >
+          {getWords(article.title, 6)}
+          {isTitleSummary && <>...</>}
+        </h3>
+        <p className="max-h-24 text-sm overflow-hidden">
+          {getWords(article.description, 20)}
+          {isDescriptionSummary && <>...</>}
         </p>
         <div className="flex items-center justify-between gap-3">
           <input
